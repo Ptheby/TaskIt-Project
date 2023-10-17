c67e7f9d39b6421b873798db919a9b5ddbd090b1
Paul Theby <paultheby@gmail.com>
2023-10-16 19:52:13 -0500

    Feat(sidebar):implemented sidebar

--- TaskIt/src/app/app.module.ts
+++ TaskIt/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    TasklistComponent
   ],
   imports: [
     BrowserModule
